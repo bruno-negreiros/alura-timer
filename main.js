8b001d4c5f148704363327f6b4ee1054897e226e
Bruno Negreiros <brunoananias_n@hotmail.com>
2020-04-09 18:40:49 -0400

    Criando a primeira janela

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
-console.log('Hello World!');
+const { app, BrowserWindow } = require('electron');
+
+app.on('ready', () => {
+    console.log('Aplicação Iniciada');
+    let mainWindow = new BrowserWindow({
+        width: 600,
+        height: 400
+    });
+
+    mainWindow.loadURL('https://www.alura.com.br');
+});
