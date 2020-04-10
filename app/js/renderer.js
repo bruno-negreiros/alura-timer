8db1e87de977b007b46fadeac932342955bb9f00
Bruno Negreiros <brunoananias_n@hotmail.com>
2020-04-10 17:43:59 -0400

    Muda botão ao clicar

--- app/js/renderer.js
+++ app/js/renderer.js
@@ -1,7 +1,14 @@
 const { ipcRenderer } = require('electron');
 
-const sobre = document.querySelector('#link-sobre');
+let sobre = document.querySelector('#link-sobre');
+let botaoPlay = document.querySelector('.botao-play');
+let images = ['./img/play-button.svg', './img/stop-button.svg'];
 
 sobre.addEventListener('click', function() {
     ipcRenderer.send('abrir-janela-sobre');
+});
+
+botaoPlay.addEventListener('click', function() {
+    images.reverse();
+    botaoPlay.src = images[0];
 });
